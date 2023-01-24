214edecdddfa4f895d65b85a84f024a5ba7ac9c4
AK <akrivit@gmail.com>
2023-01-24 15:07:40 -0500

    don't show warnings

--- dwh-101/app.js
+++ dwh-101/app.js
@@ -1,5 +1,7 @@
 import { bootServer } from "./server/server.js";
+import database from "./database/database.js"
 
 
 console.log(`\nthank you for choosing to learn stuff!\n`)
 bootServer()
+await database()
